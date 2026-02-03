9023e6b762092c2edb7d8d9b1289fcfc997069b6
Ran <xyfwdy@gmail.com>
2026-02-03 12:30:06 +0800

    移除 提示

--- src/main/java/cc/ranmc/server/network/BaseHandler.java
+++ src/main/java/cc/ranmc/server/network/BaseHandler.java
@@ -1,6 +1,5 @@
 package cc.ranmc.server.network;
 
-import cc.ranmc.server.Main;
 import cc.ranmc.server.util.CrossUtil;
 import io.javalin.http.ContentType;
 import io.javalin.http.Context;
@@ -10,6 +9,5 @@ public class BaseHandler {
         CrossUtil.allow(context);
         context.contentType(ContentType.TEXT_HTML);
         context.result("<html><head><meta http-equiv=\"refresh\" content=\"0;url=https://www.ranmc.cc\"></head><body></body></html>");
-        Main.getLogger().info("{}跳转官网", context.header("X-Forwarded-For"));
     }
 }
