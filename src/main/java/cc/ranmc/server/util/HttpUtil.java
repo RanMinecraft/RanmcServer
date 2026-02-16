4ef0bdfccd49a32890c5905a972a6d270bb930b7
Ran <xyfwdy@gmail.com>
2026-02-16 18:56:44 +0800

    适配Unirest

--- src/main/java/cc/ranmc/server/util/HttpUtil.java
+++ src/main/java/cc/ranmc/server/util/HttpUtil.java
@@ -1,64 +1,56 @@
 package cc.ranmc.server.util;
 
 import com.alibaba.fastjson2.JSONObject;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import kong.unirest.core.Unirest;
 
-import java.time.Duration;
 import java.util.function.Consumer;
+
+import static cc.ranmc.utils.BasicUtil.THY_PREFIX;
+import static cc.ranmc.utils.BasicUtil.print;
 
 public class HttpUtil {
 
-    private static final OkHttpClient client = new OkHttpClient();
-    private static final Duration TIMEOUT = Duration.ofSeconds(8);
+    private static final int TIMEOUT = 8000;
 
     public static void get(String url, Consumer<String> callback) {
-        new Thread(() -> {
-            OkHttpClient c = client.newBuilder().callTimeout(TIMEOUT).build();
-            Request request = new Request.Builder().url(url).build();
-            String result = "";
-            try (Response response = c.newCall(request).execute()) {
-                if (response.isSuccessful()) result = response.body().string();
-            } catch (Exception ignored) {}
-            callback.accept(result);
-        }).start();
+        Unirest.get(url).requestTimeout(TIMEOUT).asStringAsync()
+                .handleAsync((result, e) -> {
+                    if (e != null) {
+                        print(THY_PREFIX + "&c网路请求失败 " + e.getMessage());
+                        return "";
+                    }
+                    return result.getBody();
+                }).thenAccept(callback);
     }
 
     public static void post(String url, String body, Consumer<String> callback) {
-        new Thread(() -> {
-            OkHttpClient c = client.newBuilder().callTimeout(TIMEOUT).build();
-            RequestBody requestBody = RequestBody.create(body,
-                    MediaType.parse("application/x-www-form-urlencoded; charset=utf-8"));
-            Request request = new Request.Builder()
-                    .url(url)
-                    .post(requestBody)
-                    .build();
-            String result = "";
-            try (Response response = c.newCall(request).execute()) {
-                if (response.isSuccessful()) result = response.body().string();
-            } catch (Exception ignored) {}
-            callback.accept(result);
-        }).start();
+        Unirest.post(url)
+                .header("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+                .body(body)
+                .requestTimeout(TIMEOUT)
+                .asStringAsync()
+                .handleAsync((result, e) -> {
+                    if (e != null) {
+                        print(THY_PREFIX + "&c网路请求失败 " + e.getMessage());
+                        return "";
+                    }
+                    return result.getBody();
+                }).thenAccept(callback);
     }
 
     public static void post(String url, JSONObject body, Consumer<String> callback) {
-        new Thread(() -> {
-            OkHttpClient c = client.newBuilder().callTimeout(TIMEOUT).build();
-            RequestBody requestBody = RequestBody.create(body.toString(),
-                    MediaType.parse("application/json; charset=utf-8"));
-            Request request = new Request.Builder()
-                    .url(url)
-                    .post(requestBody)
-                    .build();
-            String result = "";
-            try (Response response = c.newCall(request).execute()) {
-                if (response.isSuccessful()) result = response.body().string();
-            } catch (Exception ignored) {}
-            callback.accept(result);
-        }).start();
+        Unirest.post(url)
+                .header("Content-Type", "application/json; charset=utf-8")
+                .body(body.toString())
+                .requestTimeout(TIMEOUT)
+                .asStringAsync()
+                .handleAsync((result, e) -> {
+                    if (e != null) {
+                        print(THY_PREFIX + "&c网路请求失败 " + e.getMessage());
+                        return "";
+                    }
+                    return result.getBody();
+                }).thenAccept(callback);
     }
 
 }
