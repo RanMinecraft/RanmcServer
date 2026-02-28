ee548cf35e2424bafc0e2179e1bcd509c1628d9c
Ran <xyfwdy@gmail.com>
2026-03-01 02:19:34 +0800

    优化 ai

--- src/main/java/cc/ranmc/server/util/MarkdownUtil.java
+++ src/main/java/cc/ranmc/server/util/MarkdownUtil.java
@@ -1,13 +1,26 @@
 package cc.ranmc.server.util;
 
-import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.gfm.tasklist.TaskListExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.data.MutableDataSet;
+import com.vladsch.flexmark.util.misc.Extension;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MarkdownUtil {
 
     public static String mdToHtml(String markdown) {
         MutableDataSet options = new MutableDataSet();
+        List<Extension> extensions = Arrays.asList(
+                TablesExtension.create(),
+                StrikethroughExtension.create(),
+                TaskListExtension.create()
+        );
+        options.set(Parser.EXTENSIONS, extensions);
         Parser parser = Parser.builder(options).build();
         HtmlRenderer renderer = HtmlRenderer.builder(options).build();
         return renderer.render(parser.parse(markdown));
