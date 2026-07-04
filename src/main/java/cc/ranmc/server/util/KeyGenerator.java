aeafaf1731c086d4ee2170ca5982b8c5bdca196e
Ran <xyfwdy@gmail.com>
2026-07-05 00:45:12 +0800

    修复 密钥生成器字符集缺少数字0

--- src/main/java/cc/ranmc/server/util/KeyGenerator.java
+++ src/main/java/cc/ranmc/server/util/KeyGenerator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class KeyGenerator {
 
-    private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyz123456789";
+    private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789";
 
     public static String get() {
         StringBuilder builder = new StringBuilder();
