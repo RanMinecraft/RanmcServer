fbbdc815711240dc08a6c38e857fe01245c9393d
Ran <xyfwdy@gmail.com>
2026-08-02 01:20:35 +0800

    优化 获取客户端真实IP的逻辑 优化 插件验证日志，添加客户端IP记录

--- src/main/java/cc/ranmc/server/network/PluginHandler.java
+++ src/main/java/cc/ranmc/server/network/PluginHandler.java
@@ -1,5 +1,6 @@
 package cc.ranmc.server.network;
 
+import cc.ranmc.server.Main;
 import cc.ranmc.server.constant.Code;
 import cc.ranmc.server.constant.Data;
 import cc.ranmc.server.constant.Prams;
@@ -15,14 +16,18 @@ public class PluginHandler {
         context.contentType(ContentType.APPLICATION_JSON);
 
         JSONObject json = new JSONObject();
+        String ip = context.header("X-Forwarded-For");
+        if (ip == null) ip = context.ip();
         if (!context.queryParamMap().containsKey(Prams.KEY) ||
                 !Data.PLUGIN_KEY.equals(context.queryParam(Prams.KEY))) {
+            Main.getLogger().warn("Plugin验证失败 IP: {}", ip);
             json.put(Prams.CODE, Code.UNKNOWN_REQUEST);
             json.put(Prams.MSG, "未验证");
             context.status(Code.UNKNOWN_REQUEST);
             context.result(json.toString());
             return;
         }
+        Main.getLogger().info("Plugin验证成功 IP: {}", ip);
         json.put(Prams.CODE, Code.SUCCESS);
         json.put(Prams.MSG, "成功");
         context.result(json.toString());
